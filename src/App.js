54354938de3a5bdd4fd4606619c23f26cd6ad774
Lucila Masini <lulym2008@hotmail.com>
2021-09-24 22:39:00 -0300

    Se crea la navbar como componente y se agrega logo de app
    * Se agrega logo.
    * Se cambia el title de la tab por "NotFlix".
    * Se eliminan las fotos que react pone por defecto.
    * Se abstrae la lógica de la navbar  a un solo archivo para poder reutilizarlo

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import Dashboard from './Views/Dashboard/Dashboard';
 import NotFound from './Views/NotFound/NotFound';
+import Navbar from '././Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route path="*" component={NotFound}/>
-        </Switch>
-      </BrowserRouter>
+      <Navbar/>
+      <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="*" component={NotFound}/>
+          </Switch>
+        </BrowserRouter>
     </div>
   );
 }
