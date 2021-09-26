40e72c3dffa89750563838cc319090f56e8d4c37
cinversi <86429942+cinversi@users.noreply.github.com>
2021-09-26 17:31:35 -0300

    Commit creacion-de-pagina-facturacion-24

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import Dashboard from './Views/Dashboard/Dashboard';
+import NotFound from './Views/NotFound/NotFound';
+import Navbar from '././Components/Navbar';
+import Invoicing from './Views/Invoicing/Invoicing';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="primary">Primary</Button>{' '}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/invoicing" component={Invoicing} />
+            <Route path="*" component={NotFound}/>
+          </Switch>
+        </BrowserRouter>
     </div>
   );
 }
