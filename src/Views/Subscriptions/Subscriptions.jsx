6f4580e8d7d6e26f3a3f409ac24cd147790dd8cc
Lucila Masini <lulym2008@hotmail.com>
2021-10-10 14:02:56 -0300

    Update Subscriptions.jsx

--- src/Views/Subscriptions/Subscriptions.jsx
+++ src/Views/Subscriptions/Subscriptions.jsx
@@ -81,13 +81,7 @@ function Subscriptions() {
                                                 <p className="packageName">{sub.name}</p>
                                             </Col>
                                             <Col sm={12} md={6} lg={6} style={{display: "flex", justifyContent:"flex-end"}}>
-                                                <Button type="primary" bsPrefix="cancel btnTextColor" 
-                                                    onClick={() => {
-                                                        setSubs(sub), 
-                                                        setModalCancelSubscriptions(true)
-                                                    }}>
-                                                    Cancelar Suscripción
-                                                </Button>
+                                                <Button type="primary" bsPrefix="cancel btnTextColor" onClick={() => {setSubs(sub), setModalCancelSubscriptions(true)}}> Cancelar Suscripción </Button>
                                             </Col>
                                         </Row>
                                         </Container>
@@ -125,7 +119,7 @@ function Subscriptions() {
                 size="md"
                 aria-labelledby="contained-modal-title-vcenter"
                 centered>
-                    
+
                 <Modal.Body style={{ border: "1px solid #C78C36"}}>
                     <p style={{textAlign:'center', fontSize:20, color:'#C78C36'}}>CANCELAR SUSCRIPCIÓN</p>
                     <br></br>
@@ -133,23 +127,8 @@ function Subscriptions() {
                     <br></br>
                     <p style={{textAlign:'center', fontSize:18, textAlign:'left', marginRight:'15px', marginLeft:'15px'}}>Por favor, presione en el boton “Aceptar” para confirmar la operación.</p>
                     <Container style={{display:'flex', justifyContent:'center'}}>
-                        <Button
-                            style={{fontSize:20, color:'#C78C36', backgroundColor: '#FFF', borderColor: '#FFF', }}
-                            onClick={() =>{
-                                setModalCancelSubscriptions(false)
-                            }}>
-                            Cancelar
-                        </Button>
-                        <Button
-                            style={{fontSize:20, color:'#C78C36', backgroundColor: '#FFF', borderColor: '#FFF'}}
-                            onClick={() => {
-                                cambiarEstadoSuscripcion(subs.userId, subs.subscriptionId, subs.packageId),
-                                setModalCancelSubscriptions(false),
-                                setModalSuccess(true),
-                                fetchMyAPI()
-                            }}>
-                            Aceptar
-                        </Button>
+                        <Button style={{fontSize:20, color:'#C78C36', backgroundColor: '#FFF', borderColor: '#FFF', }} onClick={() =>{ setModalCancelSubscriptions(false) }}>Cancelar</Button>
+                        <Button style={{fontSize:20, color:'#C78C36', backgroundColor: '#FFF', borderColor: '#FFF'}} onClick={() => { cambiarEstadoSuscripcion(subs.userId, subs.subscriptionId, subs.packageId), setModalCancelSubscriptions(false), setModalSuccess(true), fetchMyAPI() }}>Aceptar</Button>
                     </Container>
                 </Modal.Body>
             </Modal>
